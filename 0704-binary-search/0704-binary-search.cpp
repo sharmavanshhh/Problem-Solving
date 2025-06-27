9aaec5b203a8f60a38ad8dd41a9ee7e1afee0c5f
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-06-27 10:30:56 +0530

    Time: 0 ms (100%), Space: 31.3 MB (45.15%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int key) {
+        int low = 0;
+        int high = arr.size()-1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            else if(key < arr[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
