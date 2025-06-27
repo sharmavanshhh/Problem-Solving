ca788d2fa7f9cec65deaa199195fab9f944cf37c
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-06-27 10:38:01 +0530

    Time: 1 ms (61.32%), Space: 54.9 MB (74.39%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& arr) {
+        long long n = arr.size();
+        long long i = 0;
+        while(i < n){
+            if(arr[i] > 0 && arr[i] <= n && arr[i] != arr[arr[i]-1]){             
+                swap(arr[i], arr[arr[i]-1]);
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(arr[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
