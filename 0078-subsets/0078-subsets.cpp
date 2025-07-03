649945e36d21ca1f15ca4303c51f7199ac19e99c
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-07-03 12:24:27 +0530

    Time: 0 ms (100%), Space: 10.4 MB (34.57%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
-
-    // void sub(vector<int>& nums, vector<vector<int>>& result, int index, vector<int>& currSub){
-    //     if(index == nums.size()){
-    //         result.push_back(currSub);
-    //         return;
-    //     }
-
-    //     currSub.push_back(nums[index]);
-    //     sub(nums, result, index+1, currSub);
-    //     currSub.pop_back();
-    //     sub(nums, result, index+1, currSub);
-    // }
-
-    void iteration(vector<int>& nums, vector<vector<int>>& ans, int start, vector<int> temp){
+    void recursion(vector<int>& nums, vector<vector<int>>& ans, int start, vector<int> temp){
         ans.push_back(temp);
         for(int i=start; i<nums.size(); i++){
             temp.push_back(nums[i]);
-            iteration(nums, ans, i+1, temp);
+            recursion(nums, ans, i+1, temp);
             temp.pop_back();
         }
     }
@@ -25,12 +12,9 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
 
-        // vector<int> currSub;
-        // sub(nums, result, 0, currSub);
-
         vector<int> temp;
 
-        iteration(nums, result, 0, temp);
+        recursion(nums, result, 0, temp);
 
         return  result;
 
