e19e12afc3f78f0ccb75a07f32334e3e18035ed0
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-07-04 13:28:14 +0530

    Time: 3 ms (29.36%), Space: 12.1 MB (12.17%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-     void recursion(vector<int>& nums, vector<vector<int>>& ans, vector<int> temp){
+     void recursion(vector<int>& nums, vector<vector<int>>& ans, vector<int> temp, int index){
         if(temp.size() == nums.size()){
             ans.push_back(temp);
             return;
@@ -10,7 +10,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             if (find(temp.begin(), temp.end(), nums[i]) != temp.end()) continue;
             temp.push_back(nums[i]);
-            recursion(nums, ans, temp);
+            recursion(nums, ans, temp, index+1);
             temp.pop_back();
         }
     }
@@ -19,7 +19,7 @@ public:
         vector<vector<int>> result;
         vector<int> temp;
 
-        recursion(nums, result, temp);
+        recursion(nums, result, temp, 0);
 
         return result;
     }
