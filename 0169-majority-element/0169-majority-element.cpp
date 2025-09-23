b9fdbfff550f56e244528feac079b84cde3533e0
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-09-23 19:31:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int count = 0;
-        int num = -1;
+        int lead = -1;
+
         for(int i=0; i<nums.size(); i++){
             if(count == 0){
-                num = nums[i];
-                count = 1;
+                lead = nums[i];
             }
-            else if(nums[i] == num){
+            else if(nums[i] == lead){
                 count++;
             }
             else{
                 count--;
             }
         }
-        return num;
+
+        return lead;
     }
 };
