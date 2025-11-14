f4d96473d1d66a1c1e13d14102ee9aba7a2c16d9
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-11-14 21:34:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int xorr = 0;
+        
+        // XOR All
+        for(int i=0; i<nums.size(); i++){
+            xorr = xorr ^ nums[i];
+        }
+
+        // Turn on rightmost set bit
+        long long int rightmost_bit = xorr & (-xorr);
+
+        // Divide in groups
+        int g1 = 0;
+        int g2 = 0;
+        for(int x : nums){
+            if(x & rightmost_bit){
+                g1 = g1 ^ x;
+            }
+            else{
+                g2 = g2 ^ x;
+            }
+        }
+
+        return {g1, g2};
+    }
+};
