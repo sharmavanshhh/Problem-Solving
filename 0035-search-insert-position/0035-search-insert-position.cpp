f923fc3b912814dea61904c8ffb0e6b7faed1c6b
VANSH SHARMA <sharmavansh0512@gmail.com>
2025-11-18 17:06:10 +0530

    Time: 0 ms (100%), Space: 13.6 MB (76.86%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,21 +4,17 @@ public:
         int low = 0;
         int high = nums.size() - 1;
 
-        // int ans = -1;
-        int pos = -1;
-
         while(low <= high){
             int mid = low + (high - low) / 2;
+
             if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid] < target){
-                low = mid + 1;
-                // pos = mid;
+            else if(nums[mid] > target){
+                high = mid - 1;
             }
             else{
-                high = mid - 1;
-                // pos = mid;
+                low = mid + 1;
             }
         }
 
