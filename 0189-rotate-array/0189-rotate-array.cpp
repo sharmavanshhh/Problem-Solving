77d8c6220e5558a3f26edd3505405e7960234939
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-23 12:36:56 +0530

    Time: 0 ms (100%), Space: 29.6 MB (42.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int d) {
-        int n = nums.size();
-        d = d % n;
-        reverse(nums.end() - d, nums.end());
-        reverse(nums.begin(), nums.end() - d);
-        reverse(nums.begin(), nums.end());
+    void rotate(vector<int>& nums, int k) {
+       /*Brute Force Solution - O(nk)*/
+    //    int n = nums.size();
+    //    int p = 1;
+    //    while(p <= k){
+    //     int temp = nums[n-1];
+    //     for(int i=n-1; i>0; i--){
+    //         nums[i] = nums[i-1];
+    //     }
+    //     nums[0] = temp;
+    //     p++;
+    //    } 
+
+    /*Optimal Solution - O(n)*/
+    int n = nums.size();
+    k = k % n;
+    reverse(nums.end() - k, nums.end());
+    reverse(nums.begin(), nums.end() - k);
+    reverse(nums.begin(), nums.end());
     }
 };
