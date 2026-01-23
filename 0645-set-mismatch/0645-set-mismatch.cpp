70282af6c9ef55145301af7a48532e1cc960af06
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-23 10:43:12 +0530

    Time: 1 ms (57.63%), Space: 25.1 MB (89.43%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -4,22 +4,20 @@ public:
         int i = 0;
         int n = nums.size();
 
-        while (i < n) {
-            int correctIndex = nums[i] - 1;
-            if (nums[i] != nums[correctIndex]) {
-                swap(nums[i], nums[correctIndex]);
-            } else {
+        while(i < n){
+            int index = nums[i] - 1;
+            if(nums[i] != nums[index]){
+                swap(nums[i], nums[index]);
+            }
+            else{
                 i++;
             }
         }
-
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return {nums[i], i + 1}; 
+        for(int k=0; k<n; k++){
+            if(nums[k] != k+1){
+                return {nums[k], k+1};
             }
-        }   
-
-        return {}; 
+        }
+        return {};
     }
 };
