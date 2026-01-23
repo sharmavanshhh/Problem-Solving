68a5f17a61ad4fe65c18a6aba477ea0add83b875
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-23 12:24:23 +0530

    Time: 0 ms (100%), Space: 25.4 MB (79.05%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
+        vector<int> ans;
         int i = 0;
         int n = nums.size();
-        vector<int>ans;
         while(i < n){
             int index = nums[i];
             if(nums[i] != nums[index]){
@@ -13,13 +13,11 @@ public:
                 i++;
             }
         }
-
-        for(int i=0; i<n; i++){
-            if(nums[i] != i){
-                ans.push_back(nums[i]);
+        for(int k=0; k<n; k++){
+            if(nums[k] != k){
+                ans.push_back(nums[k]);
             }
         }
-
         return ans;
     }
 };
