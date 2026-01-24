1c717ce4b632f0832c71522faf90ecbd2a895f19
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-24 13:24:07 +0530

    Time: 0 ms (100%), Space: 63.5 MB (77.47%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int i = 0;
-        while(i + 1 < arr.size() && arr[i] < arr[i + 1]){
-            i++;
+        // Brute Force - [O(n)]
+        // int i = 0;
+        // while(i + 1 < arr.size() && arr[i] < arr[i + 1]){
+        //     i++;
+        // }
+        // return i;
+
+        // Optimal Solution - Binary Search [O(log n)]
+        int low = 0;
+        int high = arr.size() - 1;
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] < arr[mid + 1]){
+                low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
         }
-        return i;
+        return low;
     }
 };
