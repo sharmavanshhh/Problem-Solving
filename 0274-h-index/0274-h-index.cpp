9825ec4a89ffa6c8e058a6199fbaf049bd258a31
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-25 10:27:34 +0530

    Time: 0 ms (100%), Space: 12.2 MB (64.04%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+
+        /*Binary Search - [O(n log n)]*/
+
+        // int n = citations.size();
+        // int low = 0, high = n;
+        // while (low <= high) {
+        //     int mid = low + (high - low) / 2;
+        //     int count = 0;
+        //     for (int c : citations) {
+        //         if (c >= mid)
+        //             count++;
+        //     }
+        //     if (count >= mid)
+        //         low = mid + 1;
+        //     else
+        //         high = mid - 1;
+        // }
+        // return high;
+
+        /*Sorting Logic - [O(n log n)]*/
+        sort(citations.begin(), citations.end());
         int n = citations.size();
-        int low = 0, high = n;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            int count = 0;
-            for (int c : citations) {
-                if (c >= mid)
-                    count++;
+        for(int i=0; i<n; i++){
+            if(citations[i] >= n-i){
+                return n-i;
             }
-
-            if (count >= mid)
-                low = mid + 1;
-            else
-                high = mid - 1;
         }
-        return high;
+        return 0;
     }
 };
