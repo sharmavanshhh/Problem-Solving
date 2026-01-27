e881cca196e768c56d575b4253a56c05e5e19aab
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-27 09:12:55 +0530

    Time: 0 ms (100%), Space: 10 MB (25.12%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -14,7 +14,7 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != nullptr && fast->next != nullptr) {
+        while(fast != nullptr && fast->next != nullptr){
             slow = slow->next;
             fast = fast->next->next;
         }
