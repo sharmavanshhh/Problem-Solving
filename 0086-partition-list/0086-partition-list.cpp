72c7468900eeac446439b1dc0cd0f1d7f1c4cbc1
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-27 14:22:10 +0530

    Time: 2 ms (4.18%), Space: 15 MB (43.99%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -4,7 +4,7 @@
  *     int val;
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr   ) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
@@ -28,7 +28,6 @@ public:
             }
             head = head->next;
         }
-
         ltail->next = right->next;
         rtail->next = nullptr;
         return left->next;
