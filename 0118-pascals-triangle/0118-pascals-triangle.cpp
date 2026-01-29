4dca5e42341509f4379761ffec3a88c2995f7bd4
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-29 12:26:27 +0530

    Time: 0 ms (100%), Space: 9.7 MB (31.27%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> pascal;
-        pascal.push_back({1});
-        if(numRows == 1) return pascal; 
-        pascal.push_back({1,1}); 
-        if(numRows == 2) return pascal;
+        /*DP Soln*/
+        // vector<vector<int>> pascal;
+        // pascal.push_back({1});
+        // if(numRows == 1) return pascal;
+        // pascal.push_back({1,1});
+        // if(numRows == 2) return pascal;
 
-        for(int i=2; i<numRows; i++){
-            vector<int> row(i+1, 1);
-            for(int j=1; j<i; j++){
-                row[j] = pascal[i-1][j-1] + pascal[i-1][j];
+        // for(int i=2; i<numRows; i++){
+        //     vector<int> row(i+1, 1);
+        //     for(int j=1; j<i; j++){
+        //         row[j] = pascal[i-1][j-1] + pascal[i-1][j];
+        //     }
+        //     pascal.push_back(row);
+        // }
+        // return pascal;
+
+        /*Mathematical Approach*/
+        vector<vector<int>> pascal;
+        for (int i = 0; i < numRows; i++) {
+            vector<int> rows;
+            long long ele = 1;
+            for (int j = 0; j <= i; j++) {
+                rows.push_back(ele);
+                ele = ele * (i - j) / (j + 1);
             }
-            pascal.push_back(row); 
+            pascal.push_back(rows);
         }
         return pascal;
     }
