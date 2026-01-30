70da36b7c8c9f3c732b29144c37c2919854e11d4
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-30 15:29:10 +0530

    Time: 167 ms (70.29%), Space: 12.6 MB (65.58%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -4,20 +4,21 @@ public:
         if (n <= 2)
             return 0;
 
-        vector<char> prime(n, 1);
-        prime[0] = prime[1] = 0;
+        vector<bool> prime(n, true);
+        prime[0] = prime[1] = false;
 
         for (int i = 2; i * i < n; i++) {
             if (prime[i]) {
                 for (int j = i * i; j < n; j += i) {
-                    prime[j] = 0;
+                    prime[j] = false;
                 }
             }
         }
 
         int count = 0;
         for (int i = 2; i < n; i++) {
-            count += prime[i];
+            if (prime[i])
+                count++;
         }
 
         return count;
