c21cbc791cb4b306b8cfc77296e6d274cff76472
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-01-31 20:40:13 +0530

    Time: 21 ms (5.05%), Space: 95.2 MB (61.95%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,39 +4,40 @@ public:
         int m = nums1.size();
         int n = nums2.size();
 
-        if(m > n){
+        if (m > n) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
         int low = 0;
         int high = m;
         int part = (n + m + 1) / 2;
-        while(low <= high){
+        while (low <= high) {
             int mid1 = low + (high - low) / 2;
             int mid2 = part - mid1;
 
             int l1 = INT_MIN, l2 = INT_MIN;
             int r1 = INT_MAX, r2 = INT_MAX;
 
-            if(mid1 < m) r1 = nums1[mid1];
-            if(mid2 < n) r2 = nums2[mid2];
-            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
-            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if (mid1 < m)
+                r1 = nums1[mid1];
+            if (mid2 < n)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
 
-            if(l1 <= r2 && l2 <= r1){
-                if((m+n) % 2 != 0){
+            if (l1 <= r2 && l2 <= r1) {
+                if ((m + n) % 2 != 0) {
                     return max(l1, l2);
+                } else {
+                    return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
                 }
-                else{
-                    return (double)(max(l1,l2) + min(r1,r2)) / 2.0;
-                }
-            }
-            else if(l1 > r2){
+            } else if (l1 > r2) {
                 high = mid1 - 1;
-            }
-            else{
+            } else {
                 low = mid1 + 1;
-            }   
+            }
         }
         return 0;
     }
