05ec042d05941f84d27e57c48b798e5f3a953d96
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-02-07 09:20:11 +0530

    Time: 0 ms (100%), Space: 76.9 MB (92.22%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -16,13 +16,13 @@ public:
 
         int carry = 0;
 
-        while(l1 || l2 || carry){
+        while (l1 || l2 || carry) {
             int sum = carry;
-            if(l1){
+            if (l1) {
                 sum += l1->val;
                 l1 = l1->next;
             }
-            if(l2){
+            if (l2) {
                 sum += l2->val;
                 l2 = l2->next;
             }
