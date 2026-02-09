f2a608ce36ea26b628e89b29c40ef7af8809a864
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-02-09 09:26:52 +0530

    Time: 7 ms (40.41%), Space: 23.7 MB (80.49%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,13 +7,15 @@ public:
 
         int n = intervals.size();
 
+        ans.push_back(intervals[0]);
+
         for(int i = 1; i < n; i++){
-            if(intervals[i][0] <= intervals[i-1][1]){
-                ans.push_back({intervals[i-1][0], intervals[i][1]});
-            }
-            else{
-                ans.push_back(intervals[i]);
-            }
+           if(intervals[i][0] <= ans.back()[1]){
+            ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+           }
+           else{
+            ans.push_back(intervals[i]);
+           }
         }
         return ans;
     }
