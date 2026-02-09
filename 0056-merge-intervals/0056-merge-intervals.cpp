7290f5ccb9edeaa5f48ff3b68c7d3b53b0fe592a
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-02-09 09:26:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,19 +3,15 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
 
-        if (intervals.empty()) {
-            return ans;
-        }
+        sort(intervals.begin(), intervals.end());
 
-        sort(intervals.begin(), intervals.end());  // Sort by start time
+        int n = intervals.size();
 
-        ans.push_back(intervals[0]);
-        for (int i = 1; i < intervals.size(); i++) {
-            // Compare with last interval in ans
-            if (intervals[i][0] <= ans.back()[1]) {
-                // Merge with last interval
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            } else {
+        for(int i = 1; i < n; i++){
+            if(intervals[i][0] <= intervals[i-1][1]){
+                ans.push_back({intervals[i-1][0], intervals[i][1]});
+            }
+            else{
                 ans.push_back(intervals[i]);
             }
         }
