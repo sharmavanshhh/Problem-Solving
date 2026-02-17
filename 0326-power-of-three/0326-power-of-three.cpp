508429c4c77d050a2b8aa0e6c9af406c67972181
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-02-17 20:35:41 +0530

    Time: 0 ms (100%), Space: 9 MB (42.06%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n < 1) return false;
-
-        while(n % 3 == 0) n /= 3;
-
-        return n == 1;
+        // if(n < 1) return false;
+        // while(n % 3 == 0) n /= 3;
+        // return n == 1;
 
         // return n > 0 && 1162261467 % n == 0;
+
+        if(n == 1) return true;
+        if(n == 0 || n % 3 != 0) return false;
+
+        return isPowerOfThree(n / 3);
     }
 };
