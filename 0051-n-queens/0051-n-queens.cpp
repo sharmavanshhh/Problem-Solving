80226341473b7efe0c49948c7f9de9daf4501579
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-02-21 21:36:18 +0530

    Time: 3 ms (62.64%), Space: 10.2 MB (76.18%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> temp(n, string(n, '.'));
+        backtrack(n, ans, temp, 0);
+        return ans;
+    }
+
+    void backtrack(int n, vector<vector<string>>& ans, vector<string>& temp, int row){
+        if(row == n){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(canPlace(row, i, temp, n)){
+                temp[row][i] = 'Q';
+                backtrack(n, ans, temp, row + 1);
+                temp[row][i] = '.';
+            }
+        }
+    }
+
+    bool canPlace(int row, int col, vector<string>& temp, int n){
+        for(int j = 0; j < n; j++){
+            if(temp[row][j] == 'Q') return false;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(temp[i][col] == 'Q') return false; 
+        }
+
+        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--){
+            if(temp[i][j] == 'Q') return false;
+        }
+
+        for(int i = row, j = col; i >= 0 && j < n; i--, j++){
+            if(temp[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+};
