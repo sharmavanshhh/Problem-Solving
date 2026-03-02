c04bc2b8db0003985bd3543ff0602c313f38b187
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-02 21:23:28 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.6%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int i = 0;
+        // Cycle Sort
+
+        // int i = 0;
+        // int n = nums.size();
+        // while(i < n){
+        //     int index = nums[i];
+        //     if(nums[i] < n && nums[i] != nums[index]){
+        //         swap(nums[i], nums[index]);
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // for(int k=0; k<n; k++){
+        //     if(nums[k] != k){
+        //         return k;
+        //     }
+        // }
+        // return n;
+
+        // Bit Manipulation Trick
         int n = nums.size();
-        while(i < n){
-            int index = nums[i];
-            if(nums[i] < n && nums[i] != nums[index]){
-                swap(nums[i], nums[index]);
-            }
-            else{
-                i++;
-            }
+        int xorr = n;
+        for(int i = 0; i < n; i++){
+            xorr ^= i;
+            xorr ^= nums[i];
         }
-        for(int k=0; k<n; k++){
-            if(nums[k] != k){
-                return k;
-            }
-        }
-        return n;
+        return xorr;
     }
 };
