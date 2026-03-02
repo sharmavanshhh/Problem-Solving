6d148146b8476deb78f79de0bbe2cf2eece83a68
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-02 20:49:06 +0530

    Time: 0 ms (100%), Space: 10.3 MB (99.92%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,15 +4,27 @@ public:
         vector<int> ans(n+1, 0);
 
         // Brute-Force [O(nlog(n))]
-        for(int i=1; i<=n; i++){
-            int num = i;
-            int count = 0;
-            while(num){
-                count += num % 2;
-                num /= 2;
+        // for(int i=1; i<=n; i++){
+        //     int num = i;
+        //     int count = 0;
+        //     while(num){
+        //         count += num % 2;
+        //         num /= 2;
+        //     }
+        //     ans[i] = count;
+        // }
+        // return ans;
+
+        // Optimal Solution [O(n)]
+        for(int i = 1; i <= n; i++){
+            if(!(i & 1)){
+                ans[i] = ans[i/2];
             }
-            ans[i] = count;
+            else{
+                ans[i] = ans[i/2] + 1;
+            }
         }
+
         return ans;
     }
 };
