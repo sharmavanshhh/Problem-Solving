07b1553edc67921b4d8510f8368ff37f3bd7cc27
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-02 20:20:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.17%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-
         // Brute-Force
-        if(n <= 0) return false;
-
-        while(n % 2 == 0) {
-            n /= 2;
-        }
-        return n == 1;
-
         // if(n <= 0) return false;
+        // while(n % 2 == 0) {
+        //     n /= 2;
+        // }
+        // return n == 1;
 
-        // return (n & n-1) == 0;
+
+        // Optimal
+        if(n <= 0) return false;
+        return (n & n - 1) == 0;
     }
 };
