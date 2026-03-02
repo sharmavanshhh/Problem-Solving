d5e4e7dbb4644551f52731c5fae4d8649dac8c1c
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-02 21:28:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.6%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getSum(int a, int b) {
         while(b != 0){
-            uint32_t carry = (a & b) << 1;
+            int carry = (a & b) << 1;
             a = a ^ b;
             b = carry;
         }
