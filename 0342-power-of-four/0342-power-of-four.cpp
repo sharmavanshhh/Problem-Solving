f523fe630d9129fa3b258dfa1c514e19f3830693
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-02 20:39:06 +0530

    Time: 0 ms (100%), Space: 7.7 MB (79.18%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n > 0 &&
-               ((n & n-1) == 0) && 
-               (n & 0x55555555) != 0;      
+        if(n <= 0) return false;
+        // Check power of 2
+        if((n & (n - 1)) != 0) return false;
+        // Check even position bit
+        if((n & 0x55555555) == 0) return false;
+        return true;
+
+        // if(n < 1) return false;
+        // while(n % 4 == 0) n /= 4;
+        // return n == 1;    
     }
 };
