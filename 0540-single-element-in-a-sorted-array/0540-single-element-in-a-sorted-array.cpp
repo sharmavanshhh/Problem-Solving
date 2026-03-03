4b6dac91577e3c9aecb6c79328c9bcf420cf6bb8
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-03 20:11:11 +0530

    Time: 0 ms (100%), Space: 26.1 MB (92.42%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,15 +7,13 @@ public:
         while(low < high){
             int mid = low + (high - low) / 2;
 
-            if(mid % 2 != 0) mid--;
+            if(mid & 1) mid--;
 
-            if(nums[mid] == nums[mid+1]){
-                low = mid + 2;
-            }
-            else{
-                high = mid;
-            }
+            if(nums[mid] == nums[mid + 1]) low = mid + 2;
+
+            else high = mid;
         }
+
         return nums[low];
     }
 };
