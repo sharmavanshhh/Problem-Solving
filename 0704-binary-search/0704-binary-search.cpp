3b2b7513e869a7d0e3fa79706395a27b8621465d
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-03 19:47:05 +0530

    Time: 0 ms (100%), Space: 31.2 MB (80.16%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,15 +5,9 @@ public:
         int high = nums.size() - 1;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] < target){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
         return -1;
     }
