b4424828119c50f5367c37ceeb3d615d5834fb92
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-06 17:24:19 +0530

    Time: 7 ms (91.01%), Space: 35.1 MB (53.72%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -6,6 +6,7 @@ public:
 
         while(low <= high){
             int mid = low + (high - low) / 2;
+
             int numberOfDays = isShippable(weights, mid);
 
             if(numberOfDays <= days){
@@ -15,21 +16,22 @@ public:
                 low = mid + 1;
             }
         }
+
         return low;
     }
 
     int isShippable(vector<int>& weights, int capacity){
-        int day = 1;
+        int days = 1;
         int load = 0;
         for(int w : weights){
             if(load + w > capacity){
-                day++;
+                days++;
                 load = w;
             }
             else{
                 load += w;
             }
         }
-        return day;
+        return days;
     }
 };
