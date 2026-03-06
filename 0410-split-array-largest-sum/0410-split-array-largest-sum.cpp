c27dbae4a3342388b79feffaf48dfadd72ab437e
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-06 20:10:23 +0530

    Time: 0 ms (100%), Space: 10.4 MB (58.78%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -4,33 +4,30 @@ public:
         int low = *max_element(nums.begin(), nums.end());
         int high = accumulate(nums.begin(), nums.end(), 0);
 
-        while(low < high){
+        while(low <= high){
             int mid = low + (high - low) / 2;
 
-            int pieces = countPieces(nums, mid);
+            int splits = canSplit(nums, mid);
 
-            if(pieces > k){
-                low = mid + 1;
-            }
-            else{
-                high = mid;
-            }
+            if(splits <= k) high = mid - 1;
+            else low = mid + 1;
         }
+
         return low;
     }
 
-    int countPieces(vector<int>& nums, int mid){
-        int pieces = 1;
-        int sum = 0;
+    int canSplit(vector<int>& nums, int maxSum){
+        int split = 1;
+        int currSum = 0;
         for(int x : nums){
-            if(sum + x > mid){
-                pieces++;
-                sum = x;
+            if(currSum + x > maxSum){
+                split++;
+                currSum = x;
             }
             else{
-                sum += x;
+                currSum += x;
             }
         }
-        return pieces;
+        return split;
     }
 };
