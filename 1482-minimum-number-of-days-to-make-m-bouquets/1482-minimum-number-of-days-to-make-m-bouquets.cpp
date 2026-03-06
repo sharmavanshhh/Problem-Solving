ec5ebde27dc236f50113ec7fff3add276ec0459c
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-06 19:25:14 +0530

    Time: 23 ms (54.95%), Space: 70.4 MB (76.52%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        if ((long long)m * k > bloomDay.size()){
-            return -1;
-        }
-        
+        if((long long)m * k > bloomDay.size()) return -1;
+
         int low = *min_element(bloomDay.begin(), bloomDay.end());
         int high = *max_element(bloomDay.begin(), bloomDay.end());
 
-        while (low < high) {
+        while(low < high){
             int mid = low + (high - low) / 2;
 
-            int bouquets = possibleBouquets(bloomDay, mid, k);
+            int bouquets = canMake(bloomDay, k, mid);
 
-            if (bouquets < m) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
+            if(bouquets < m) low = mid + 1;
+            else high = mid;
         }
+
         return low;
     }
 
-    int possibleBouquets(vector<int>& bloomDays, int days, int k) {
-        int bouquets = 0;
+    int canMake(vector<int>& bloomDay, int k, int days){
+        int bouquet = 0;
         int flowers = 0;
-        for (int d : bloomDays) {
-            if (d <= days) {
+
+        for(int d : bloomDay){
+            if(d <= days){
                 flowers++;
-                if (flowers == k) {
-                    bouquets++;
+                if(flowers == k){
+                    bouquet++;
                     flowers = 0;
                 }
-            } else {
+            }
+            else{
                 flowers = 0;
             }
         }
-        return bouquets;
+
+        return bouquet;
     }
 };
