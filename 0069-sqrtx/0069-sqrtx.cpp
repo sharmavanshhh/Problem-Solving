2534853c43437f3469989cea869bd35019b0cfbc
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-06 16:28:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.9%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,17 +5,14 @@ public:
         int high = x;
         int ans = 0;
         while(low <= high){
-            long long sqrt = low + (high - low) / 2;
-            if(sqrt * sqrt == x){
-                return sqrt;
-            }
-            else if(sqrt * sqrt < x){
+            int sqrt = low + (high - low) / 2;
+            long long temp = (long long)sqrt * (long long)sqrt;
+            if(temp == x) return sqrt;
+            else if(temp < x){
                 ans = sqrt;
                 low = sqrt + 1;
             }
-            else{
-                high = sqrt - 1;
-            }
+            else high = sqrt - 1;
         }
         return ans;
     }
