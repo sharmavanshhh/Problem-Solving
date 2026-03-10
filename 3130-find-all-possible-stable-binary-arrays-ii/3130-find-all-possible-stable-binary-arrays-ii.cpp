83299f54ed5ac98a05c185d604b1dc29ddae71ce
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-10 17:51:54 +0530

    Time: 147 ms (69.86%), Space: 176.8 MB (50.68%) - LeetHub

---
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+        const int MOD = 1e9 + 7;
+
+        vector<vector<long long>> dp0(zero + 1, vector<long long>(one + 1));
+        vector<vector<long long>> dp1(zero + 1, vector<long long>(one + 1));
+
+        for(int i = 1; i <= min(zero, limit); i++) dp0[i][0] = 1;
+        for(int j = 1; j <= min(one, limit); j++) dp1[0][j] = 1;
+
+        for(int i = 1; i <= zero; i++){
+            for(int j = 1; j <= one; j++){
+
+                dp0[i][j] = dp0[i-1][j] + dp1[i-1][j];
+                if(i-limit-1 >= 0)
+                    dp0[i][j] = (dp0[i][j] - dp1[i-limit-1][j] + MOD) % MOD;
+                else
+                    dp0[i][j] %= MOD;
+
+                dp1[i][j] = dp0[i][j-1] + dp1[i][j-1];
+                if(j-limit-1 >= 0)
+                    dp1[i][j] = (dp1[i][j] - dp0[i][j-limit-1] + MOD) % MOD;
+                else
+                    dp1[i][j] %= MOD;
+            }
+        }
+
+        return (dp0[zero][one] + dp1[zero][one]) % MOD;
+    }
+};
