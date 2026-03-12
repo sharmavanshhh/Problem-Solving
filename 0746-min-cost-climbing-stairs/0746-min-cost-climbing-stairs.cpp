3d405e1ba8be35093c0b984c44a34e8d9f8d35e6
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-12 13:24:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,12 +1,31 @@
+// class Solution {
+// public:
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         int n = cost.size();
+//         vector<int> dp(n + 1);
+//         dp[0] = dp[1] = 0;
+//         for(int i = 2; i <= n; i++){
+//             dp[i] = min((dp[i - 1] + cost[i - 1]), (dp[i - 2] + cost[i - 2]));
+//         }
+//         return dp[n];
+//     }
+// };
+
+
 class Solution {
 public:
+
+    int solve(int i, vector<int>& cost){
+        if(i <= 1) return 0;
+
+        int oneStep = solve(i - 1, cost) + cost[i - 1];
+        int twoStep = solve(i - 2, cost) + cost[i - 2];
+
+        return min(oneStep, twoStep);
+    }
+
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n + 1);
-        dp[0] = dp[1] = 0;
-        for(int i = 2; i <= n; i++){
-            dp[i] = min((dp[i - 1] + cost[i - 1]), (dp[i - 2] + cost[i - 2]));
-        }
-        return dp[n];
+        return solve(n, cost);
     }
 };
