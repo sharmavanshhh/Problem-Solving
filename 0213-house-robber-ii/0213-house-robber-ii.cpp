b728c512759d64d8986db18bf07fa938a51fb319
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-12 14:21:32 +0530

    Time: 0 ms (100%), Space: 10.9 MB (18.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int robLiner(vector<int>& nums, int start, int end){
+    int robLinear(vector<int>& nums, int start, int end) {
         int n = end - start + 1;
         if(n == 0) return 0;
         if(n == 1) return nums[start];
-        if(n == 2) return max(nums[start], nums[start+1]);
-
-        int* dp = new int[n];
+        if(n == 2) return max(nums[start], nums[start + 1]);
+        
+        vector<int> dp(n);
         dp[0] = nums[start];
-        dp[1] = max(nums[start], nums[start+1]);
-        for(int i=2; i<n; i++){
-            dp[i] = max(dp[i-1], dp[i-2]+nums[start+i]);
+        dp[1] = max(nums[start], nums[start + 1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = max(dp[i - 1], nums[start+i] + dp[i - 2]);
         }
-        return dp[n-1];
+        
+        return dp[n - 1];
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
 
-        return max(robLiner(nums, 0, n-2), robLiner(nums, 1, n-1));
+        if(n == 1) return nums[0];
+        
+        int case1 = robLinear(nums, 0, n - 2);
+        int case2 = robLinear(nums, 1, n - 1);
+
+        return max(case1, case2);
     }
 };
