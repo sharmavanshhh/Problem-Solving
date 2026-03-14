70c52d77a7648a9482a2f59ad6113797da58abb7
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-14 12:43:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,27 +1,62 @@
+// class Solution {
+// public:
+//     string getHappyString(int n, int k) {
+//         vector<string> happy;
+//         generate(happy, "", 0, n);
+
+//         sort(happy.begin(), happy.end());
+
+//         if(k > happy.size()) return "";
+
+//         return happy[k-1];
+//     }
+
+//     void generate(vector<string>& happy, string curr, int index, int n){
+//         if(index == n){
+//             happy.push_back(curr);
+//             return;
+//         }
+
+//         for(char ch : {'a', 'b', 'c'}){
+//             if(curr.empty() || curr.back() != ch){
+//                 generate(happy, curr + ch, index + 1, n);
+//             }
+//         }
+//     }
+
+// };
+
+
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        vector<string> happy;
-        generate(happy, "", 0, n);
 
-        sort(happy.begin(), happy.end());
+        int total = 3 * (1 << (n-1));
+        if(k > total) return "";
 
-        if(k > happy.size()) return "";
+        string ans = "";
+        char prev = '#';
 
-        return happy[k-1];
-    }
+        for(int i = 0; i < n; i++){
 
-    void generate(vector<string>& happy, string curr, int index, int n){
-        if(index == n){
-            happy.push_back(curr);
-            return;
-        }
+            for(char ch : {'a','b','c'}){
 
-        for(char ch : {'a', 'b', 'c'}){
-            if(curr.empty() || curr.back() != ch){
-                generate(happy, curr + ch, index + 1, n);
+                if(ch == prev) continue;
+
+                int remaining = n - i - 1;
+                int block = 1 << remaining;
+
+                if(k > block){
+                    k -= block;
+                }
+                else{
+                    ans += ch;
+                    prev = ch;
+                    break;
+                }
             }
         }
-    }
 
+        return ans;
+    }
 };
