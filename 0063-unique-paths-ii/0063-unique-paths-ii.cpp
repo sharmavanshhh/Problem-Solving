934f9c88a8268e2f3d8a6212bcb7a84b176fcc04
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-15 20:15:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int solve(vector<vector<int>>& obstacleGrid, int i, int j, int m, int n,
               vector<vector<int>>& dp) {
-        
-        if (i < m && j < n && obstacleGrid[i][j] == 1)
-            return 0;
-
-        if (i == m - 1 && j == n - 1)
-            return 1;
-
         if (i >= m || j >= n)
             return 0;
+        
+        if (obstacleGrid[i][j] == 1)
+            return 0;
+        
+        if (i == m - 1 && j == n - 1)
+            return 1;
 
         if (dp[i][j] != -1)
             return dp[i][j];
