f5fc2994999c5748758c6eb1a461f808680d2529
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-19 19:21:14 +0530

    Time: 38 ms (75.03%), Space: 81.8 MB (50.71%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        sort(
-            envelopes.begin(), envelopes.end(),
-            [](vector<int> &a, vector<int> &b){
-                if(a[0] == b[0]){
-                    return a[1] > b[1];
-                }
-                return a[0] < b[0];
-            }
-        );
+        
+        sort(envelopes.begin(), envelopes.end(),
+             [](vector<int>& a, vector<int>& b) {
+                 if (a[0] == b[0])
+                     return a[1] > b[1];
+                 return a[0] < b[0];
+             });
 
-        vector<int> lis;
-       
-       for(auto &env : envelopes){
-        int h = env[1];
+        vector<int> temp;
 
-        int left = 0;
-        int right = lis.size() - 1;
-        int pos = lis.size();  
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(lis[mid] >= h){
-                pos = mid;
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
-        }
-        if(pos == lis.size())
-            lis.push_back(h);
-        else
-            lis[pos] = h;
+        for(auto &env : envelopes){
+            int h = env[1];
+
+            auto it = lower_bound(temp.begin(), temp.end(), h);
+
+            if(it == temp.end())
+                temp.push_back(h);
+            else
+                *it = h;
         }
 
-        return lis.size();
+        return temp.size();
     }
 };
