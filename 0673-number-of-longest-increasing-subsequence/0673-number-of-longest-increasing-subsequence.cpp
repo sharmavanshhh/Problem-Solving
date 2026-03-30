37366e653b9a51010c9e40423724236e380de5b8
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-03-30 18:55:46 +0530

    Time: 25 ms (78.08%), Space: 17.6 MB (10.25%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    pair<int, int> solve(vector<int>& nums, int index, int prevIndex,
-                         vector<vector<pair<int,int>>>& dp) {
-
-        if(index == nums.size()) return {0, 1};
-
-        if(dp[index][prevIndex + 1].first != -1)
-            return dp[index][prevIndex + 1];
-
-        auto skip = solve(nums, index + 1, prevIndex, dp);
-
-        pair<int, int> take = {0, 0};
-
-        if(prevIndex == -1 || nums[index] > nums[prevIndex]){
-            auto next = solve(nums, index + 1, index, dp);
-            take.first = 1 + next.first;
-            take.second = next.second;
-        }
-
-        if(take.first > skip.first)
-            return dp[index][prevIndex + 1] = take;
-
-        if(skip.first > take.first)
-            return dp[index][prevIndex + 1] = skip;
-
-        return dp[index][prevIndex + 1] =
-            make_pair(take.first, take.second + skip.second);
-    }
-
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<pair<int,int>>> dp(n, vector<pair<int,int>>(n+1, {-1, -1}));
+        vector<int> dp(n, 1);      // length of LIS ending at i
+        vector<int> count(n, 1);   // number of LIS ending at i
 
-        return solve(nums, 0, -1, dp).second;
+        int maxi = 1;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    if(dp[j] + 1 > dp[i]){
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    }
+                    else if(dp[j] + 1 == dp[i]){
+                        count[i] += count[j];
+                    }
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            if(dp[i] == maxi)
+                ans += count[i];
+        }
+
+        return ans;
     }
 };
