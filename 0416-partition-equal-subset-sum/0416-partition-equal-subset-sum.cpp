17179046ca01b37ad26d8794796ca15b3899f23c
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-02 16:59:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,3 +1,37 @@
+// class Solution {
+// public:
+//     bool canPartition(vector<int>& nums) {
+//         int sum = 0;
+//         for(int x : nums) sum += x;
+
+//         if(sum % 2 != 0) return false;
+
+//         int target = sum / 2;
+//         int n = nums.size();
+
+//         vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+
+//         // base case
+//         for(int i = 0; i <= n; i++) dp[i][0] = true;
+
+//         for(int i = 1; i <= n; i++){
+//             for(int j = 0; j <= target; j++){
+
+//                 bool skip = dp[i-1][j];
+//                 bool take = false;
+
+//                 if(nums[i-1] <= j){
+//                     take = dp[i-1][j - nums[i-1]];
+//                 }
+
+//                 dp[i][j] = skip || take;
+//             }
+//         }
+
+//         return dp[n][target];
+//     }
+// };
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
@@ -7,27 +41,16 @@ public:
         if(sum % 2 != 0) return false;
 
         int target = sum / 2;
-        int n = nums.size();
 
-        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+        vector<bool> dp(target+1, false);
+        dp[0] = true;
 
-        // base case
-        for(int i = 0; i <= n; i++) dp[i][0] = true;
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 0; j <= target; j++){
-
-                bool skip = dp[i-1][j];
-                bool take = false;
-
-                if(nums[i-1] <= j){
-                    take = dp[i-1][j - nums[i-1]];
-                }
-
-                dp[i][j] = skip || take;
+        for(int num : nums){
+            for(int j = target; j >= num; j--){
+                dp[j] = dp[j] || dp[j - num];
             }
         }
 
-        return dp[n][target];
+        return dp[target];
     }
 };
