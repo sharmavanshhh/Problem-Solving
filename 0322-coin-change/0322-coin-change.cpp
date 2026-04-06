c904476f51ac7719e4456bb017831155c6c663d1
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-06 21:22:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        // int n = coins.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
+        // for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= amount; j++){
+        //         int skip = dp[i-1][j];
+        //         int take = 1e9;
+        //         if(coins[i-1] <= j){
+        //             take = 1 + dp[i][j - coins[i-1]];
+        //         }
+        //         dp[i][j] = min(skip, take);
+        //     }
+        // }
+        // return dp[n][amount] == 1e9 ? -1 : dp[n][amount];
+
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
-
-        for(int i = 0; i <= n; i++) dp[i][0] = 0;
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= amount; j++){
-                int skip = dp[i-1][j];
-                int take = 1e9;
-                if(coins[i-1] <= j){
-                    take = 1 + dp[i][j - coins[i-1]];
-                }
-                dp[i][j] = min(skip, take);
+        vector<int> dp(amount + 1, 1e9);
+        dp[0] = 0;
+        for(int coin : coins){
+            for(int j = coin; j <= amount; j++){
+                dp[j] = min(dp[j],1 + dp[j - coin]);
             }
         }
-
-        return dp[n][amount] == 1e9 ? -1 : dp[n][amount];
+        return dp[amount] == 1e9 ? -1 : dp[amount];
     }
 };
