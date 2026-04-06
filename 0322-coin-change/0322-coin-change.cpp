d7c84adc747bcf42e48a7474ade225a0a5a59023
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-06 21:08:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int solve(int index, int amount, vector<int>& coins, vector<vector<int>>& dp){
-        if(amount == 0) return 0;
-        if(index == coins.size()) return 1e9;
-
-        if(dp[index][amount] != -1) return dp[index][amount];
-
-        int skip = solve(index + 1, amount, coins, dp);
-
-        int take = 1e9;
-
-        if(coins[index] <= amount){
-            take = 1 + solve(index, amount - coins[index], coins, dp);
-        }
-
-        return dp[index][amount] = min(skip, take);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
         int ans = solve(0, amount, coins, dp);
-        return ans >= 1e9 ? -1 : ans;
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= amount; j++){
+                int skip = dp[i-1][j];
+                int take = 1e9;
+                if(coins[i-1] <= j){
+                    take = 1 + dp[i][j - coins[i-1]];
+                }
+                dp[i][j] = min(skip, take);
+            }
+        }
+
+        return dp[n][amount] == 1e9 ? -1 : dp[n][amount];
     }
 };
