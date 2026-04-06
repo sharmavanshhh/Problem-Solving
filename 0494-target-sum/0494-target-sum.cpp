9c59865926a59c2bbcd1b047d4f10578e37af5d7
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-06 16:45:28 +0530

    Time: 6 ms (69.36%), Space: 15.6 MB (43.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int ans = 0;
-        backtrack(nums, target, ans, 0, 0);
-        return ans;
+        int n = nums.size();
+
+        int total = 0;
+        for(int x : nums) total += x;
+
+        if((target + total < 0 ) || ((target + total) & 1)) return 0;
+
+        int sum = (target + total) / 2;
+
+        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
+
+        return solve(0, sum, nums, dp);
     }
 
-    void backtrack(vector<int>& nums, int target, int& ans, long long sum, int index){
+    int solve(int index, int sum, vector<int>& nums, vector<vector<int>>& dp){
         if(index == nums.size()){
-            if(sum == target) ans++;
-            return;
+            return (sum == 0) ? 1 : 0;
         }
 
-        backtrack(nums, target, ans, sum + (long long)nums[index], index + 1);
-        backtrack(nums, target, ans, sum - (long long)nums[index], index + 1);
+        if(dp[index][sum] != -1) return dp[index][sum];
+
+        int skip = solve(index + 1, sum, nums, dp);
+        int take = 0;
+        if(nums[index] <= sum){
+            take = solve(index + 1, sum - nums[index], nums, dp);
+        }
+
+        return dp[index][sum] = skip + take;
     }
 };
