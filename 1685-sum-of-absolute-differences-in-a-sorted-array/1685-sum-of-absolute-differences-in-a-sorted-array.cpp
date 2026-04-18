5abb9f7ee8dfb7ae6a2e4564ce1fb6f14a22e71b
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-18 20:35:17 +0530

    Time: 85 ms (23.15%), Space: 87.2 MB (91.86%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -5,17 +5,15 @@ public:
         vector<int> ans(n);
 
         int totalSum = 0;
-        for(int x : nums){
-            totalSum += x;
-        }
+        for(int x : nums) totalSum += x;
 
         int leftSum = 0;
 
         for(int i = 0; i < nums.size(); i++){
             long long rightSum = totalSum - leftSum - nums[i];
 
-            int leftPart = (long long)nums[i] * i - leftSum;
-            int rightPart = rightSum - (long long)nums[i] * (n - i - 1);
+            int leftPart = (long long) nums[i] * i - leftSum;
+            int rightPart = rightSum - (long long) nums[i] * (n - i - 1);
 
             ans[i] = leftPart + rightPart;
 
