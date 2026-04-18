1ba04ce417a104b9d98f97bd3edf38c48b1462d4
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-18 18:34:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
+
+        const int MOD = 1e9 + 7;
         vector<int> dp(amount + 1, 0);
         dp[0] = 1;
+
         for(int coin : coins){
             for(int j = coin; j <= amount; j++){
-                dp[j] += dp[j - coin];
+                dp[j] = (dp[j] + dp[j - coin]) % MOD;
             }
         }
+
         return dp[amount];
     }
 };
