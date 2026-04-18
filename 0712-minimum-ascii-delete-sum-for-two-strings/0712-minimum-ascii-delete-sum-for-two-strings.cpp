aa6912dded816513cb03683774835cfce3a67c42
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-18 19:35:23 +0530

    Time: 15 ms (73.19%), Space: 18.1 MB (71.77%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        int m = s1.length();
-        int n = s2.length();
+        int m = s1.size();
+        int n = s2.size();
 
-        int c = 0;
         int total = 0;
-        for (char ch : s1) total += ch;
-        for (char ch : s2) total += ch;
+        for(char ch : s1) total += ch;
+        for(char ch : s2) total += ch;
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
 
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
                 if(s1[i-1] == s2[j-1]){
                     dp[i][j] = dp[i-1][j-1] + s1[i-1];
-                    c += s1[i-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-
         int common = dp[m][n];
-        return total - 2*common;
+        return total - 2 * common;
     }
 };
