2d115f688791d266aee27d203c96ab05e4265577
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-18 19:58:02 +0530

    Time: 84 ms (19.84%), Space: 146.3 MB (52.49%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,23 +3,18 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
         mp[0] = -1;
-
         int sum = 0;
-
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
-
             int rem = sum % k;
-
             if(mp.count(rem)){
-                if(i - mp[rem] >= 2){
-                    return true;
-                }
+                if(i - mp[rem] >= 2) return true;
             }
             else{
                 mp[rem] = i;
             }
         }
+
         return false;
     }
 };
