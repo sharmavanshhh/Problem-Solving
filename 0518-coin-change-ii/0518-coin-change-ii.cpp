e25457315edad48c20de335b4f686c51a611a127
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-18 18:27:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<long long>> dp(n+1, vector<long long>(amount+1, 0));
-        for(int i = 0; i <= n; i++) dp[i][0] = 1;
-        for(int i = 1; i <= n; i++){
-            for(int j = 0; j <= amount; j++){
-                int skip = dp[i-1][j];
-                int take = 0;
-                if(coins[i-1] <= j){
-                    take = dp[i][j - coins[i-1]];
-                }
-
-                dp[i][j] = (long long)skip + (long long)take;
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int j = coin; j <= amount; j++){
+                dp[j] += dp[j - coin];
             }
         }
-        return dp[n][amount];
+        return dp[amount];
     }
 };
