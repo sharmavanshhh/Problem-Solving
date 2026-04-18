164daba54d8b59840078d3e9dcea9e768955ca12
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-18 18:11:55 +0530

    Time: 43 ms (29.16%), Space: 37.2 MB (23.16%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int index, int amount, vector<int>& coins, vector<vector<int>>& dp){
+        if(amount == 0) return 1;
+        if(index == coins.size()) return 0;
+
+        if(dp[index][amount] != -1) return dp[index][amount];
+
+        int skip = solve(index + 1, amount, coins, dp);
+
+        int take = 0;
+        if(coins[index] <= amount){
+            take = solve(index, amount - coins[index], coins, dp);
+        }
+
+        return dp[index][amount] = skip + take;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        return solve(0, amount, coins, dp);
+    }
+};
