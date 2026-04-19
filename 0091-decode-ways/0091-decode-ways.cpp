a9143b7acaced9cd41f2a9d110acf196cab423a6
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-19 21:59:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int solve(int index, string& s, vector<int>& dp){
-        if(index == s.size()) return 1;
-        if(s[index] == '0') return 0;
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; i--){
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
 
-        if(dp[index] != -1) return dp[index];
+            dp[i] = dp[i+1];
 
-        int ways = solve(index+1, s, dp);
-
-        if(index + 1 < s.size()){
-            int num = (s[index] - '0') * 10 + (s[index+1] - '0');
-            if(num >= 10 && num <= 26){
-                ways += solve(index+2, s, dp);
+            if(i+1 < n){
+                int num = (s[i] - '0') * 10 + (s[i+1] - '0');
+                if(num >= 10 && num <= 26){
+                    dp[i] += dp[i+2];
+                }
             }
         }
 
-        return dp[index] = ways;
-    }
-
-    int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n+1, -1);
-        return solve(0, s, dp);
+        return dp[0];
     }
 };
