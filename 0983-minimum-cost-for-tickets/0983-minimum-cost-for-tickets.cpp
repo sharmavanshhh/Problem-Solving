66986eaa1b7fa0af0244b6565059b9ec81c791e9
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-21 19:30:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& days, vector<int>& costs, vector<int>& dp){
-        if(index >= days.size()) return 0;
-
-        if(dp[index] != -1) return dp[index];
-
-        int daily = costs[0] + solve(index + 1, days, costs, dp);
-
-        int j = index;
-        while(j < days.size() && days[j] < days[index] + 7) j++;
-        int weekly = costs[1] + solve(j, days, costs, dp);
-
-        j = index;
-        while(j < days.size() && days[j] < days[index] + 30) j++;
-        int monthly = costs[2] + solve(j, days, costs, dp);
-
-        return dp[index] = min({daily, weekly, monthly});
-    }
-
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int d = days.size();
         int c = costs.size();
-        vector<int> dp(d, -1);
-        return solve(0, days, costs, dp);
+        vector<int> dp(d + 1, 0);
+
+        for(int i = d - 1; i >= 0; i--){
+            int daily = costs[0] + dp[i+1];
+
+            int j = i;
+            while(j < d && days[j] < days[i] + 7) j++;
+            int weekly = costs[1] + dp[j];
+
+            j = i;
+            while(j < d && days[j] < days[i] + 30) j++;
+            int monthly = costs[2] + dp[j];
+
+            dp[i] = min({daily, weekly, monthly});
+        }
+
+        return dp[0];
     }
 };
