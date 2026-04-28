3f2fc361b47d669d79512df1f9553e24de16f275
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-28 18:14:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -20,14 +20,12 @@ public:
 
         while(!q.empty()){
             double size = q.size();
-            vector<int> level;
             double sum = 0;
             for(int i = 0; i < size; i++){
                 TreeNode* node = q.front();
                 q.pop();
 
                 sum += node->val;
-                level.push_back(node->val);
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
