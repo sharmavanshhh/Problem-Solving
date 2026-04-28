8c5cfa41a70fac8e3f824a0906861f623a2d5f9b
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-28 17:04:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p & !q) return true;
+        if(!p && !q) return true;
         if(!p || !q) return false;
         if(p->val != q->val) return false;
 
