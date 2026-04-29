b31f1551386d989103acdfe2970b4100795c1cfa
VANSH SHARMA <sharmavansh0512@gmail.com>
2026-04-29 15:01:06 +0530

    Time: 23 ms (10.11%), Space: 18.7 MB (99.96%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -6,32 +6,29 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, long long currSum, int target, int& count){
-        if(!root){
-            return;
-        }
-        currSum += root->val;
-        if(currSum == target){
-            count++;
-        }
-        dfs(root->left, currSum, target, count);
-        dfs(root->right, currSum, target, count);
-    }
-    int pathSum(TreeNode* root, int targetSum) {
-        if (!root) return 0;
+    int countPaths(TreeNode* node, long long targetSum) {
+        if (!node)
+            return 0;
 
         int count = 0;
+        if (node->val == targetSum)
+            count++;
 
-        // Start DFS from every node
-        dfs(root, 0, targetSum, count);
-        count += pathSum(root->left, targetSum);
-        count += pathSum(root->right, targetSum);
+        count += countPaths(node->left, targetSum - node->val);
+        count += countPaths(node->right, targetSum - node->val);
 
         return count;
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(!root) return 0;
+
+        return countPaths(root, targetSum) + pathSum(root->left, targetSum) +
+               pathSum(root->right, targetSum);
     }
 };
